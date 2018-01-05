f5dd997334607308f7eae3bc22ec66f7f79e2676
zeneli <lidozeneli@gmail.com>
2018-01-05 15:14:53 -0500

    blog: Define Post-Startup and Post-Tag model relationships.
    A many-to-many relationship between blog posts and startups.
    Blog Posts may be about multiple Startups, just as Startups may be
    written about multiple times.
    
    A many-to-many relationship between blog posts and tags.
    Posts may also be categorized by multiple Tags, just as Tags may be
    used multiple times to categorize different Posts.

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-
+from organizer.models import Startup, Tag
 
 class Post(models.Model):
     title = models.CharField(max_length=63)
     slug = models.SlugField()
     text = models.TextField()
     pub_date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    startups = models.ManyToManyField(Startups)
