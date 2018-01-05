2ee9324cb3c0dd292043a62b0df9a6975f436c38
zeneli <lidozeneli@gmail.com>
2018-01-05 14:43:01 -0500

    organizer: Declare and define Tag, Startup, and NewsLink models.

--- organizer/models.py
+++ organizer/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=31)
+    slug = models.SlugField()
+
+
+class Startup(models.Model):
+    name = models.CharField(max_length=31)
+    slug = models.SlugField()
+    description = models.TextField()
+    founded_date = models.DateField()
+    contact = models.EmailField()
+    website = models.URLField()
+
+
+class NewsLink(models.Model):
+    title = models.CharField(max_length=63)
+    pub_date = models.DateField()
+    link = models.URLField()
