c7002c29b3edd97da2e9fd6f824b5489f7fdd076
zeneli <lidozeneli@gmail.com>
2018-01-08 12:54:08 -0500

    homepage: Render Tag list view template.

--- organizer/views.py
+++ organizer/views.py
@@ -1,9 +1,13 @@
 from django.http.response import HttpResponse
+from django.template import loader
 
 from .models import Tag
 
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    output = ", ".join([tag.name for tag in tag_list])
+    template = loader.get_template(
+        'organizer/tag_list.html')
+    context = {'tag_list': tag_list}
+    output = template.render(context)
     return HttpResponse(output)
