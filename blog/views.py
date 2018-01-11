36d8d4379a00147ceb34d579e3de7fd665746984
zeneli <lidozeneli@gmail.com>
2018-01-10 22:22:31 -0500

    Post List: Add post list view, template, and URL pattern.
    Post list view renders post_list.html template with all the posts.
    Blog URL Config patterns are encapsulated and included inside the project URL Config pattern mapping in a blog namespace.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+    template = loader.get_template('blog/post_list.html')
+    output = template.render({'post_list': post_list})
+    return HttpResponse(output)
