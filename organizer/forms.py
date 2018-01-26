0b652b82101f64dfe0f67cd927c635cc5f9ccd9a
zeneli <lidozeneli@gmail.com>
2018-01-25 20:38:06 -0500

    TagForm: Add clean_slug method to clean the slug fied.

--- organizer/forms.py
+++ organizer/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Tag
 
@@ -8,6 +9,19 @@ class TagForm(forms.Form):
     slug = forms.SlugField(
         max_length=31,
         help_text='A label for URL config')
+
+    # clean_name ensures the name field is lowercase.
+    def clean_name(self):
+        return self.cleaned_data['name'].lower()
+
+    # clean_slug must do two things:
+    # 1) ensure slug is lowercase (to prevent alphabetization and conflicts)
+    # 2) ensure slug is not create (to prevent URL path collision)
+    def clean_slug(self):
+        new_slug = (slef.cleaned_data['slug'].lower())
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "create".')
+        return new_slug
 
     def save(self):
         new_tag = Tag.objects.create(
