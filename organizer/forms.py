fa39f92499b04a346c2cc2888bb546160c14f580
zeneli <lidozeneli@gmail.com>
2018-01-25 23:04:48 -0500

    TagForm: Refactor TagForm to inherit from ModelForm.

--- organizer/forms.py
+++ organizer/forms.py
@@ -4,11 +4,14 @@ from django.core.exceptions import ValidationError
 from .models import Tag
 
 
-class TagForm(forms.Form):
-    name = forms.CharField(max_length=31)
-    slug = forms.SlugField(
-        max_length=31,
-        help_text='A label for URL config')
+# TagForm inherits from forms.ModelForm so we can connect it to the
+# corresponding Tag model. Helps maintain DRY principle.
+class TagForm(forms.ModelForm):
+    # Connect TagForm to Tag model.
+    # No longer need field names and save method.
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
     # clean_name ensures the name field is lowercase.
     def clean_name(self):
@@ -22,9 +25,3 @@ class TagForm(forms.Form):
         if new_slug == 'create':
             raise ValidationError('Slug may not be "create".')
         return new_slug
-
-    def save(self):
-        new_tag = Tag.objects.create(
-            name=self.cleaned_data['name'],
-            slug=self.cleaned_data['slug'])
-        return new_tag
