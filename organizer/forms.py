9b66ceff8aa40401cf37349dbef40f66f7485fb5
zeneli <lidozeneli@gmail.com>
2018-01-26 09:28:49 -0500

    organizer form: Create SlugCleanMixin and refactor TagFrom and StartupForm.

--- organizer/forms.py
+++ organizer/forms.py
@@ -6,7 +6,9 @@ from .models import NewsLink, Startup, Tag
 
 # TagForm inherits from forms.ModelForm so we can connect it to the
 # corresponding Tag model. Helps maintain DRY principle.
-class TagForm(forms.ModelForm):
+# TagForm inherits from SlugCleanMixin in order to mix the
+# clean_slug functionality into the TagForm.
+class TagForm(SlugCleanMixin, forms.ModelForm):
     # Connect TagForm to Tag model.
     # No longer need field names and save method.
     class Meta:
@@ -17,30 +19,24 @@ class TagForm(forms.ModelForm):
     def clean_name(self):
         return self.cleaned_data['name'].lower()
 
-    # clean_slug must do two things:
-    # 1) ensure slug is lowercase (to prevent alphabetization and conflicts)
-    # 2) ensure slug is not create (to prevent URL path collision)
-    def clean_slug(self):
-        new_slug = (slef.cleaned_data['slug'].lower())
-        if new_slug == 'create':
-            raise ValidationError('Slug may not be "create".')
-        return new_slug
 
-
-class StartupForm(forms.ModelForm):
+class StartupForm(SlugCleanMixin, forms.ModelForm):
     class Meta:
         model = Startup
         fields = '__all__'
-
-    # clean_slug (copy of TagForm's clean_slug)
-    def clean_slug(self):
-        new_slug = (slef.cleaned_data['slug'].lower())
-        if new_slug == 'create':
-            raise ValidationError('Slug may not be "create".')
-        return new_slug
 
 
 class NewsLinkForm(forms.ModelForm):
     class Meta:
         model = Startup
         fields = '__all__'
+
+
+class SlugCleanMixin:
+    """Mixin class for slug cleaning method."""
+
+    def clean_slug(self):
+        new_slug = (slef.cleaned_data['slug'].lower())
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "create".')
+        return new_slug
